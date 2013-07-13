52842cce16183e58dc331b022543de9984ef5de5
pcrane <trigunshin@gmail.com>
2013-07-13 02:47:46 -0400

    moving formatting and marionette along. added forms library for next step.

--- src/public/js/marionnette-demo.js
+++ src/public/js/marionnette-demo.js
@@ -1,12 +1,11 @@
-MyApp = new Backbone.Marionette.Application();
-MyApp.addRegions({
-    mainRegion: "#content"
-});
-
 Bookmark = Backbone.Model.extend({
     idAttribute: '_id',
-    defaults: {url: 'default_url'},
-    urlRoot: "/api/bookmark/"
+    //defaults: {url: 'default_url'},
+    urlRoot: "/api/bookmark/",
+    schema: {
+        url: 'Text',
+        description: 'Text'
+    }
 });
 Bookmarks = Backbone.Collection.extend({
     model: Bookmark,
@@ -14,7 +13,12 @@ Bookmarks = Backbone.Collection.extend({
 });
 
 Group = Backbone.Model.extend({
-    idAttribute: '_id'
+    idAttribute: '_id',
+    urlRoot: "/api/group/",
+    schema: {
+        name: 'Text',
+        description: 'Text'
+    }
 });
 Groups = Backbone.Collection.extend({
     model: Group,
@@ -29,20 +33,14 @@ BookmarkView = Backbone.Marionette.ItemView.extend({
     template: "#bookmark-template",
     tagName: "li",
     events: {
-        'click li': 'logInfoUrl',
         'click span.bookmark__delete': 'removeBookmark'
-    },
-    logInfoUrl: function(){
-        console.log(this.model.get('url'));
     },
     removeBookmark: function() {
         var self = this;
-        if(this.model.get('0')) {
-            this.model.set('_id', this.model.get('0')._id);
-        }
         this.model.destroy();
     }
 });
+
 
 GroupView = Backbone.Marionette.CompositeView.extend({
     template: "#accordion-group-template",
@@ -50,41 +48,80 @@ GroupView = Backbone.Marionette.CompositeView.extend({
     itemView: BookmarkView,
     itemViewContainer: "ul",
     events: {
-        //'click a': 'logInfoUrl',
-        'click button#bookmark_add': "addBookmark",
+        //'click span#bookmark_add': "addBookmark",
     },
     initialize: function(){
         this.collection = this.model.get('bookmarks');
     },
-    logInfoUrl: function(){
-        //console.log(this.model.get('url'));
+    appendHtml: function(collectionView, itemView){
+        collectionView.$("ul").append(itemView.el);
+    },
+});
+
+// A Grid Row (group-level)
+var GridRow = Backbone.Marionette.CompositeView.extend({
+    template: "#row-accordion-template",
+    tagName: "div",
+    className: "grid_row",
+    itemView: BookmarkView,
+    itemViewContainer: "ul",
+
+    initialize: function(){
+        this.collection = this.model.get('bookmarks');
+    },
+    events: {
+        'click span#group_remove': 'removeGroup',
+        'click span#bookmark_add': "addBookmark"
+    },
+    removeGroup: function(e) {
+        //XXX this leaves potential zombie bookmarks
+        this.model.destroy();
     },
     addBookmark: function() {
-        var cur = new Bookmark({url:'new url!'});
+        console.log("group id:"+this.model.get('_id'));
+        var cur = new Bookmark({url:'new url!', group_id:this.model.get('_id')});
         this.collection.create(cur, {wait:true});
         console.log("new_bookmark.save:"+JSON.stringify(this.collection));
     }
 });
 
-AccordionView = Backbone.Marionette.CollectionView.extend({
-    id: "groupList",
-    className: "accordion",
-    itemView: GroupView
-});
+// The grid view (group-level)
+var GridView = Backbone.Marionette.CompositeView.extend({
+    tagName: "div",
+    template: "#grid-template",
+    itemView: GridRow,
 
+    appendHtml: function(collectionView, itemView){
+        collectionView.$("div#grid_list").append(itemView.el);
+    },
+    events: {
+        'click span#group_add': 'addGroup',
+    },
+    addGroup: function(e) {
+        console.log("creating");
+        this.collection.create(
+            {name:"New Group N", description: "A new group!", _id: null},
+            {wait: true}
+        );
+    }
+});
+/*
+MyApp = new Backbone.Marionette.Application();
+MyApp.addRegions({
+    mainRegion: "#content"
+});
 MyApp.addInitializer(function(options){
     var groups = new Groups();
     groups.fetch({
         success: function() {
             // each group's bookmarks must be a backbone collection
-            // we initialize them here
             groups.each(function(group){
                 bmarks = new Bookmarks();
                 bmarks.fetch({
                     data: $.param({group_id: group._id})
                 });
                 var bookmarkCollection = bmarks;
-                group.set('bookmarks', bmarks);
+                //group.set('bookmarks', bmarks);
             });
 
             var accordionView = new AccordionView({
@@ -95,3 +132,33 @@ MyApp.addInitializer(function(options){
     });
 });
 MyApp.start();
+//*/
+
+
+var groups = new Groups();
+groups.fetch({
+    success: function() {
+        var gridView = new GridView({
+            collection: groups
+        });
+        groups.each(function(group){
+            bmarks = new Bookmarks();
+            bmarks.fetch({
+                data: $.param({group_id: group.get('_id')})
+            });
+            var bookmarkCollection = bmarks;
+            group.set('bookmarks', bmarks);
+        });
+        gridView.render();
+
+
+        var form = new Backbone.Form({
+            model: new Group({}),
+            idprefix: 'group-'
+        }).render();
+
+        $('div#group_form').append(form.el);
+
+        $("#grid").html(gridView.el);
+    }
+});
